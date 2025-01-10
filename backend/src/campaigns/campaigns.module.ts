e95919ad658dd8ad75da4dde3ff3ad773cd5dd90
andrewindeche <indecheandrew@gmail.com>
2025-01-10 11:22:33 +0300

    add service and controller imports

--- backend/src/campaigns/campaigns.module.ts
+++ backend/src/campaigns/campaigns.module.ts
@@ -1,4 +1,9 @@
 import { Module } from '@nestjs/common';
+import { CampaignsService } from './service/campaigns.service';
+import { CampaignController } from './controller/campaign.controller';
 
-@Module({})
+@Module({
+  providers: [CampaignsService],
+  controllers: [CampaignController]
+})
 export class CampaignsModule {}
