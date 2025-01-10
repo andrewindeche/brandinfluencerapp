8d5696f5981acfb8effe04d5558ed0b0de9dff34
andrewindeche <indecheandrew@gmail.com>
2025-01-10 14:31:32 +0300

    add sign up route

--- backend/src/auth/controller/auth.controller.ts
+++ backend/src/auth/controller/auth.controller.ts
@@ -1,10 +1,17 @@
 import { Controller, Post, Body } from '@nestjs/common';
 import { AuthService } from '../service/auth.service';
 import { InfluencerLoginDto } from '../../campaigns/dto/influencer-login.dto';
+import { Influencer } from '../schema/influencer.schema';
 
 @Controller('auth')
 export class AuthController {
     constructor(private authService: AuthService) {}
+
+    @Post('register')
+    async register(@Body() body: { username: string, email: string, password: string }): Promise<Influencer> {
+      const { username, email, password } = body;
+      return this.authService.register(username, email, password);
+    }
 
     @Post('login')
     async login(@Body() influencerLoginDto: InfluencerLoginDto) {
