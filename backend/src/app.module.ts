1cdb78f7974dc11977254fa51a91dc055e25870a
andrewindeche <indecheandrew@gmail.com>
2025-01-10 10:08:02 +0300

    add mongoose to modules

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -1,10 +1,13 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { MongooseModule } from '@nestjs/mongoose';
 import { CampaignsModule } from './campaigns/campaigns.module';
 
 @Module({
-  imports: [CampaignsModule],
+  imports: [
+    MongooseModule.forRoot('mongodb://localhost/nest_campaigns'),
+    CampaignsModule],
   controllers: [AppController],
   providers: [AppService],
 })
