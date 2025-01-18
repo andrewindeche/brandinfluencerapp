326c1b453add58d3a070613b265115078129208c
andrewindeche <indecheandrew@gmail.com>
2025-01-18 20:01:59 +0300

    add header

--- frontend/src/pages/index.tsx
+++ frontend/src/pages/index.tsx
@@ -1,8 +1,8 @@
 import React from 'react';
  const HomePage: React.FC = () => { 
   return ( 
-    <div className="bg-[#FF2C2C] h-screen flex justify-center items-center"> 
-      <h1 className="text-white text-4xl font-bold">Welcome to My Next.js App!</h1> 
+    <div className="bg-[#FF2C2C] h-screen flex flex-col justify-start items-center"> 
+      <h1 className="text-white text-6xl font-bold mt-10">AFFLUENCER</h1> 
     </div> 
     ); 
     }; 
