5b5659f4a5efc4f3ac635d5641841811ec58418d
andrewindeche <indecheandrew@gmail.com>
2025-01-18 20:24:35 +0300

    underline header

--- frontend/src/pages/index.tsx
+++ frontend/src/pages/index.tsx
@@ -2,7 +2,8 @@ import React from 'react';
  const HomePage: React.FC = () => { 
   return ( 
     <div className="bg-[#FF2C2C] h-screen flex flex-col justify-start items-center"> 
-      <h1 className="text-white text-6xl font-bold mt-10">AFFLUENCER</h1> 
+      <h1 className="text-white text-6xl font-bold mt-10 custom-underline">AFFLUENCER</h1>
+      <h2></h2>
     </div> 
     ); 
     }; 
