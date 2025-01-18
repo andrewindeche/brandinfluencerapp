401b79a5a5ad81535b2ad28624c1384e531b528f
andrewindeche <indecheandrew@gmail.com>
2025-01-18 21:08:25 +0300

    add kaushan script font

--- frontend/src/pages/_app.tsx
+++ frontend/src/pages/_app.tsx
@@ -1,6 +1,6 @@
 import '../styles/globals.css'
 import type { AppProps } from 'next/app'
-import { Irish_Grover, Joti_One } from "next/font/google"
+import { Irish_Grover, Joti_One ,Kaushan_Script} from "next/font/google"
 
 const irishGrover = Irish_Grover({
     weight: '400',
@@ -13,11 +13,17 @@ const irishGrover = Irish_Grover({
     subsets: ['latin'],
     variable: '--font-joti-one',
   });
+
+  const kaushanScript = Kaushan_Script({
+    weight: '400',
+    subsets: ['latin'],
+    variable: '--font-kaushan-script',
+  });
   
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
-    <main className={`${irishGrover.variable} ${jotiOne.variable}`}>
+    <main className={`${irishGrover.variable} ${jotiOne.variable} ${kaushanScript.variable}`}>
     <Component {...pageProps} />
         </main>
     );
--- frontend/src/pages/index.tsx
+++ frontend/src/pages/index.tsx
@@ -3,7 +3,7 @@ import React from 'react';
   return ( 
     <div className="bg-[#FF2C2C] h-screen flex flex-col justify-start items-center"> 
       <h1 className="text-white text-6xl font-bold mt-10 custom-underline">AFFLUENCER</h1>
-      <h2></h2>
+      <h2>Influencer MarketPlace</h2>
     </div> 
     ); 
     }; 
