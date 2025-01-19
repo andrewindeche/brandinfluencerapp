301bb58cd94ce7beb2283040d6209009056f0fa1
andrewindeche <indecheandrew@gmail.com>
2025-01-19 21:01:30 +0300

    add prettier to dependencies

--- frontend/src/pages/index.tsx
+++ frontend/src/pages/index.tsx
@@ -1,9 +1,13 @@
 import React from 'react';
  const HomePage: React.FC = () => { 
   return ( 
-    <div className="bg-[#FF2C2C] h-screen flex flex-col justify-start items-center"> 
+    <div className="bg-[#FF2C2C] h-screen flex flex-col justify-start items-center">
+      <div className="flex flex-row justify-center items-center w-full mt-10"> 
+        <div className="w-1/2 flex justify-center">
       <h1 className="text-white text-6xl font-bold mt-10 custom-underline">AFFLUENCER</h1>
-      <h2>Influencer MarketPlace</h2>
+      <h2 className="text-white text-2xl mt-6 custom-underline">Influencer MarketPlace</h2>
+      </div>
+      </div>
     </div> 
     ); 
     }; 
