c3fa138bf75fc367cd9843caf0f4352f37ffdd60
andrewindeche <indecheandrew@gmail.com>
2025-01-19 22:00:11 +0300

    update style for header

--- frontend/src/pages/_app.tsx
+++ frontend/src/pages/_app.tsx
@@ -1,32 +1,33 @@
-import '../styles/globals.css'
-import type { AppProps } from 'next/app'
-import { Irish_Grover, Joti_One ,Kaushan_Script} from "next/font/google"
+import '../styles/globals.css';
+import type { AppProps } from 'next/app';
+import { Irish_Grover, Joti_One, Kaushan_Script } from 'next/font/google';
 
 const irishGrover = Irish_Grover({
-    weight: '400',
-    subsets: ['latin'],
-    variable: '--font-irish-grover',
-  });
-  
-  const jotiOne = Joti_One({
-    weight: '400',
-    subsets: ['latin'],
-    variable: '--font-joti-one',
-  });
+  weight: '400',
+  subsets: ['latin'],
+  variable: '--font-irish-grover',
+});
 
-  const kaushanScript = Kaushan_Script({
-    weight: '400',
-    subsets: ['latin'],
-    variable: '--font-kaushan-script',
-  });
-  
+const jotiOne = Joti_One({
+  weight: '400',
+  subsets: ['latin'],
+  variable: '--font-joti-one',
+});
+
+const kaushanScript = Kaushan_Script({
+  weight: '400',
+  subsets: ['latin'],
+  variable: '--font-kaushan-script',
+});
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
-    <main className={`${irishGrover.variable} ${jotiOne.variable} ${kaushanScript.variable}`}>
-    <Component {...pageProps} />
-        </main>
-    );
+    <main
+      className={`${irishGrover.variable} ${jotiOne.variable} ${kaushanScript.variable}`}
+    >
+      <Component {...pageProps} />
+    </main>
+  );
 }
 
-export default MyApp
+export default MyApp;
