c9e6b55de7046eab38e56ba83f4b7702baab174c
andrewindeche <indecheandrew@gmail.com>
2025-01-22 15:44:31 +0300

    update button color

--- frontend/src/pages/brand.tsx
+++ frontend/src/pages/brand.tsx
@@ -6,7 +6,7 @@ const BrandPage: React.FC = () => {
   return (
     <div className="bg-[#005B96] min-h-screen flex flex-col justify-start items-center">
       <div className="flex justify-center space-x-4 mt-2">
-        <button className="bg-yellow-500 text-black py-2 px-14 rounded-full border-2 border-black">
+        <button className="bg-[#FFFF00] text-black py-2 px-14 rounded-full border-2 border-black">
           Influencers
         </button>
         <button className="bg-red-500 text-white py-2 px-14 rounded-full border-2 border-white relative">
