0fb1e9d44c5b2ce48dd98e31dbadb5820898c333
andrewindeche <indecheandrew@gmail.com>
2025-01-23 14:54:51 +0300

    add hover effect on buttons

--- frontend/src/pages/brand.tsx
+++ frontend/src/pages/brand.tsx
@@ -33,10 +33,10 @@ const BrandPage: React.FC = () => {
   return (
     <div className="bg-[#005B96] min-h-screen flex flex-col justify-start items-center">
       <div className="flex justify-center space-x-4 mt-4">
-        <button className="bg-[#FFFF00] text-black py-2 px-14 rounded-full border-2 border-black">
+        <button className="bg-[#FFFF00] hover:scale-105 text-black py-2 px-14 rounded-full border-2 border-black">
           Influencers
         </button>
-        <button className="bg-red-500 text-white py-2 px-14 rounded-full border-2 border-white relative">
+        <button className="bg-red-500 hover:scale-105 text-white py-2 px-14 rounded-full border-2 border-white relative">
           Campaigns
         </button>
       </div>
