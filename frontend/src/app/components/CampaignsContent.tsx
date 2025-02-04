02ba43ff2afd0ae29b556ad224f67f1ff9faab58
andrewindeche <indecheandrew@gmail.com>
2025-02-04 14:51:08 +0300

    add table for campaign metrics

--- frontend/src/app/components/CampaignsContent.tsx
+++ frontend/src/app/components/CampaignsContent.tsx
@@ -2,9 +2,35 @@ import React from 'react';
 
 const CampaignsContent: React.FC = () => {
   return (
-    <div className="mt-4 flex justify-center items-center space-y-12 sm:space-y-2 sm:space-x-20 w-full px-4">
-      {/* Placeholder content for campaigns */}
-      <div className="text-white text-xl">Campaigns Content Goes Here</div>
+    <div className="mt-4 flex justify-center items-center space-y-4 sm:space-y-2 w-full px-16">
+      <div className="p-4 rounded-lg shadow-lg border border-white">
+        <div className="grid grid-cols-3 gap-4 text-center text-white">
+          <div>
+            <h4 className="text-xs">Ambassadors</h4>
+            <p className="text-base">12</p>
+          </div>
+          <div>
+            <h4 className="text-xs">Total reach</h4>
+            <p className="text-base">9.8K</p>
+          </div>
+          <div>
+            <h4 className="text-xs">Submissions</h4>
+            <p className="text-base">20</p>
+          </div>
+          <div>
+            <h4 className="text-xs">Posts</h4>
+            <p className="text-base">30k</p>
+          </div>
+          <div>
+            <h4 className="text-xs">Likes</h4>
+            <p className="text-base">5k</p>
+          </div>
+          <div>
+            <h4 className="text-xs">Comments</h4>
+            <p className="text-base">60.2k</p>
+          </div>
+        </div>
+      </div>
     </div>
   );
 };
--- frontend/src/pages/brand.tsx
+++ frontend/src/pages/brand.tsx
@@ -7,6 +7,7 @@ const BrandPage: React.FC = () => {
   const [activeTab, setActiveTab] = useState<'influencers' | 'campaigns'>(
     'influencers',
   );
+
   const influencers: Influencer[] = [
     {
       name: 'APRIL',
@@ -66,7 +67,7 @@ const BrandPage: React.FC = () => {
       </div>
 
       <div
-        className={`mt-4 flex flex-col sm:flex-row justify-center items-center space-y-12 sm:space-y-2 sm:space-x-20 w-full px-4 fade-in ${activeTab === 'influencers' ? 'show' : ''}`}
+        className={`mt-4 flex flex-col sm:flex-row justify-center items-center space-y-12 sm:space-y-2 sm:space-x-20 w-full px-4 fade-in ${activeTab === 'influencers' ? 'show' : 'show'}`}
       >
         {activeTab === 'influencers' ? (
           influencers.map((influencer, index) => (
