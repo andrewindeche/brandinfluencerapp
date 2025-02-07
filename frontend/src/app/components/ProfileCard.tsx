e24f3404d41392e5d95de506701630a2082686da
andrewindeche <indecheandrew@gmail.com>
2025-02-07 13:43:10 +0300

    Update style

--- frontend/src/app/components/ProfileCard.tsx
+++ frontend/src/app/components/ProfileCard.tsx
@@ -15,12 +15,20 @@ const ProfileCard: React.FC = () => {
       </div>
 
       <div className="text-center p-4">
-        <h3 className="text-lg font-bold mb-2">H & M Brands</h3>
-        <h4 className="text-sm font-medium mb-2">About</h4>
-        <p className="text-xs leading-relaxed">
-          Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed do
-          eiusmod tempor incididunt ut labore et dolore magna aliqua.
-        </p>
+        <h3 className="text-sm font-bold mb-2">H & M Brands</h3>
+        <h4 className="text-sm custom-underline font-medium mb-2">About</h4>
+
+        <div className="relative max-h-28 overflow-hidden hover:overflow-auto hover:scrollbar-thin hover:scrollbar-thumb-yellow-500 hover:scrollbar-track-gray-800 transition-all duration-300">
+          <p className="text-xs leading-relaxed">
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed do
+            eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim
+            ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut
+            aliquip ex ea commodo consequat. Duis aute irure dolor in
+            reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla
+            pariatur. Excepteur sint occaecat cupidatat non proident, sunt in
+            culpa qui officia deserunt mollit anim id est laborum.
+          </p>
+        </div>
       </div>
     </div>
   );
@@ -28,7 +36,7 @@ const ProfileCard: React.FC = () => {
 
 const StatsCard: React.FC = () => {
   return (
-    <div className="border border-white bg-black text-white rounded-lg shadow-lg w-full max-w-xl mx-auto mt-2 p-3 flex justify-around">
+    <div className="border border-white bg-black text-white rounded-lg shadow-lg w-full max-w-xl mx-auto mt-2 p-2 flex justify-around">
       <div className="text-center">
         <p className="text-sm font-bold">30k</p>
         <p className="text-xs text-gray-400">Posts</p>
