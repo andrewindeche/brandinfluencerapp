748fe00eed00522edc25fe8d809219c52cc0502d
andrewindeche <indecheandrew@gmail.com>
2025-02-11 15:33:23 +0300

    update style

--- frontend/src/pages/influencer.tsx
+++ frontend/src/pages/influencer.tsx
@@ -26,15 +26,21 @@ const InfluencerPage: React.FC = () => {
                 height={150}
                 className="w-full h-auto rounded-lg"
               />
-              <div className="absolute bottom-4 left-1/2 transform -translate-x-1/2 text-lg font-bold bg-[#333333] text-white p-2 rounded-full z-10">
-                BRAD
+              <div className="absolute transform -translate-x-1/2 rotate-12 bg-opacity-30 px-20 py-3 bottom-6 left-1/2 transform -translate-x-1/2 text-lg bg-[#333333] text-white p-2 rounded-full z-10">
+                <span className="inline-block transform -rotate-12 text-white">
+                  BRAD
+                </span>
               </div>
-              <div className="absolute top-4 right-4 text-center z-10">
-                <div className="bg-[#333333] text-white text-xs font-bold p-3 rounded-full">
-                  100 likes
+              <div className="absolute top-4 left-1 text-center z-10">
+                <div className="bg-[#333333] rotate-12 bg-opacity-30 text-white text-xs font-bold p-3 rounded-full">
+                  <span className="inline-block transform -rotate-12 text-white">
+                    100 likes
+                  </span>
                 </div>
-                <div className="bg-[#333333] text-white text-xs font-bold p-3 mt-1 rounded-full">
-                  50 shares
+                <div className="bg-[#333333] rotate-12 bg-opacity-30 text-white text-xs font-bold p-3 mt-1 rounded-full">
+                  <span className="inline-block transform -rotate-12 text-white">
+                    50 shares
+                  </span>
                 </div>
               </div>
             </div>
@@ -52,16 +58,16 @@ const InfluencerPage: React.FC = () => {
           </div>
           <div className="border border-white bg-black text-white rounded-xl shadow-lg w-full max-w-xl mx-auto mt-1 p-1 flex justify-around">
             <div className="text-center">
-              <p className="text-xl font-bold">10</p>
-              <p className="text-sm">Campaigns</p>
+              <p className="text-3xl font-bold">10</p>
+              <p className="text-xs">Campaigns</p>
             </div>
             <div className="text-center">
-              <p className="text-xl font-bold">8</p>
-              <p className="text-sm">Posts</p>
+              <p className="text-3xl font-bold">8</p>
+              <p className="text-xs">Posts</p>
             </div>
             <div className="text-center">
-              <p className="text-xl font-bold">17</p>
-              <p className="text-sm">Submissions</p>
+              <p className="text-3xl font-bold">17</p>
+              <p className="text-xs">Submissions</p>
             </div>
           </div>
         </div>
@@ -72,7 +78,7 @@ const InfluencerPage: React.FC = () => {
             {['Campaign 1', 'Campaign 2', 'Campaign 3'].map((title, index) => (
               <div
                 key={index}
-                className={`bg-white p-4 rounded-xl shadow-lg transform transition duration-300 ${
+                className={`relative p-4 rounded-xl shadow-lg transform transition duration-300 ${
                   activeCard === title
                     ? 'scale-105 ring-4 ring-blue-500'
                     : 'hover:scale-105 hover:ring-2 hover:ring-blue-300'
@@ -82,29 +88,32 @@ const InfluencerPage: React.FC = () => {
                 <Image
                   src="/images/fit.jpg"
                   alt={title}
-                  width={150}
+                  width={200}
                   height={200}
-                  className="rounded-lg w-full mb-4"
+                  className="rounded-t-lg w-full h-[200px] object-cover"
                 />
-                <p className="font-bold">{title}</p>
-                <p className="text-gray-600">16/01/2025</p>
-                <p className="text-gray-300">
-                  I hope this message finds you well! My name is [Your Name]
-                  from [Your Brand]...
-                </p>
-                <p className="font-semibold text-blue-500 mt-2">
-                  Deadline: 2 weeks
-                </p>
-                <p
-                  className={`font-bold ${
-                    index % 2 === 0 ? 'text-green-500' : 'text-red-500'
-                  }`}
-                >
-                  {index % 2 === 0 ? 'Active' : 'Inactive'}
-                </p>
+                <div className="bg-[#0A74DA] text-white p-3 rounded-b-lg">
+                  <p className="font-bold">{title}</p>
+                  <p className="text-xs">16/01/2025</p>
+                  <p className="text-xs mt-2">
+                    I hope this message finds you well! My name is [Your Name]
+                    from [Your Brand]...
+                  </p>
+                  <p className="text-xs font-semibold mt-2">
+                    Deadline: 2 weeks
+                  </p>
+                  <p
+                    className={`text-xs font-bold mt-2 ${
+                      index % 2 === 0 ? 'text-green-400' : 'text-red-400'
+                    }`}
+                  >
+                    {index % 2 === 0 ? 'Active' : 'Inactive'}
+                  </p>
+                </div>
               </div>
             ))}
           </div>
+
           <div className="bg-[#FFD700] p-4 rounded-2xl shadow-lg">
             <h2 className="text-3xl font-bold mb-4 text-center">
               Notifications
