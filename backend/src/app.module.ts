e316ac1ec3601c083a323183a2b68447de236cc3
andrewindeche <indecheandrew@gmail.com>
2025-02-13 08:00:51 +0300

    Add admin module to dependencies

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -4,11 +4,12 @@ import { AppService } from './app.service';
 import { MongooseModule } from '@nestjs/mongoose';
 import { CampaignsModule } from './campaigns/campaigns.module';
 import { AuthModule } from './auth/auth.module';
+import { AdminModule } from './admin/admin.module';
 
 @Module({
   imports: [
     MongooseModule.forRoot('mongodb://localhost/nest_campaigns'),
-    CampaignsModule,AuthModule],
+    CampaignsModule,AuthModule, AdminModule],
   controllers: [AppController],
   providers: [AppService],
 })
