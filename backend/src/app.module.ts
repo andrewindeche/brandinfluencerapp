334c9a7dc6fc6a0f96022dab872f7edf1a6aad3f
andrewindeche <indecheandrew@gmail.com>
2025-02-17 14:58:42 +0300

    dockerize nest js

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -8,14 +8,17 @@ import { CampaignsModule } from './campaigns/campaigns.module';
 import { AuthModule } from './auth/auth.module';
 import { AdminModule } from './admin/admin.module';
 import { UserModule } from './user/user.module';
-import { User, UserSchema } from './user/user.schema'; 
 
 AdminJS.registerAdapter(MongooseAdapter);
 
 @Module({
   imports: [
     MongooseModule.forRoot('mongodb://localhost/nest_campaigns'),
-    CampaignsModule,AuthModule, AdminModule, UserModule],
+    CampaignsModule,
+    AuthModule,
+    AdminModule,
+    UserModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
