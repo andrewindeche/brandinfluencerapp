f1416d728a6f1373c42c220b89a0a50809efd637
andrewindeche <indecheandrew@gmail.com>
2025-02-26 17:24:38 +0300

    resolve imports

--- backend/src/admin/admin.module.ts
+++ backend/src/admin/admin.module.ts
@@ -1,12 +1,13 @@
 import { Module } from '@nestjs/common';
 import { AuthModule } from '../auth/auth.module';
 import { CampaignsModule } from '../campaigns/campaigns.module';
+import { UserModule } from 'src/user/user.module';
 import { BrandModule } from '../user/brand/brand.module';
 import { AdminController } from './admin.controller';
 import { AdminService } from './admin.service';
 
 @Module({
-  imports: [AuthModule, CampaignsModule, BrandModule],
+  imports: [AuthModule, UserModule, CampaignsModule, BrandModule],
   controllers: [AdminController],
   providers: [AdminService],
 })
