7ac8aba779bf69aacbf2b0b5debffd38e0e0cc86
andrewindeche <indecheandrew@gmail.com>
2025-02-26 20:57:37 +0300

    update influencer schema

--- backend/src/user/influencer/influencer.schema.ts
+++ backend/src/user/influencer/influencer.schema.ts
@@ -1,14 +1,37 @@
 import { Schema, Document } from 'mongoose';
-
-export const InfluencerSchema = new Schema({
-  username: { type: String, required: true, unique: true },
-  email: { type: String, required: true, unique: true },
-  password: { type: String, required: true },
-});
+import { Schema as MongooseSchema } from 'mongoose';
+import { Submission } from '../../auth/schema/submission.schema';
 
 export interface Influencer extends Document {
-  id: string;
+  name: string;
   username: string;
-  email: string;
   password: string;
+  email: string;
+  socialMediaHandles: {
+    instagram?: string;
+    youtube?: string;
+    twitter?: string;
+  };
+  category: string;
+  bio: string;
+  location: string;
+  submissions: Submission[];
+  campaign?: MongooseSchema.Types.ObjectId;
 }
+
+export const InfluencerSchema = new Schema<Influencer>({
+  name: { type: String, required: true },
+  username: { type: String, required: true, unique: true },
+  password: { type: String, required: true },
+  email: { type: String, required: true, unique: true },
+  socialMediaHandles: {
+    instagram: { type: String },
+    youtube: { type: String },
+    twitter: { type: String },
+  },
+  category: { type: String, required: true },
+  bio: { type: String, required: true },
+  location: { type: String, required: true },
+  submissions: [{ type: MongooseSchema.Types.ObjectId, ref: 'Submission' }],
+  campaign: { type: MongooseSchema.Types.ObjectId, ref: 'Campaign' },
+});
