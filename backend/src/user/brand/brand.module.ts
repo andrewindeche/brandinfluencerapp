b27a6e11f92491fee99c7ddc4ed9e1182f28d0a8
andrewindeche <indecheandrew@gmail.com>
2025-03-13 23:22:34 +0300

    import user module

--- backend/src/user/brand/brand.module.ts
+++ backend/src/user/brand/brand.module.ts
@@ -2,10 +2,12 @@ import { Module } from '@nestjs/common';
 import { MongooseModule } from '@nestjs/mongoose';
 import { BrandSchema } from './schema/brand.schema';
 import { BrandService } from './brand.service';
-import { CampaignsModule } from 'src/campaigns/campaigns.module';
+import { CampaignsModule } from '../../campaigns/campaigns.module';
+import { UserModule } from '../../user/user.module';
 
 @Module({
   imports: [
+    UserModule,
     MongooseModule.forFeature([{ name: 'Brand', schema: BrandSchema }]),
     CampaignsModule,
   ],
