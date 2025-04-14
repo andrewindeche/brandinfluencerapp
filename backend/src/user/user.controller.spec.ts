304f41cabe78c74b1aabaefb2a49655124c4d20f
andrewindeche <indecheandrew@gmail.com>
2025-04-14 14:40:52 +0300

    type set user object

--- backend/src/user/user.controller.spec.ts
+++ backend/src/user/user.controller.spec.ts
@@ -2,6 +2,7 @@ import { Test, TestingModule } from '@nestjs/testing';
 import { UserController } from './user.controller';
 import { UserService } from './user.service';
 import { User } from './user.schema';
+import * as mongoose from 'mongoose';
 import { ConflictException } from '@nestjs/common';
 
 describe('UserController', () => {
@@ -29,14 +30,14 @@ describe('UserController', () => {
 
   describe('findAll', () => {
     it('should return an array of users', async () => {
-      const result: User[] = [
+      const result: jest.Mocked<User>[] = [
         {
           email: 'test@example.com',
           username: 'testuser',
           role: 'influencer',
           password: '',
           _id: new mongoose.Types.ObjectId().toString(),
-        },
+        } as unknown as jest.Mocked<User>,
       ];
       jest.spyOn(service, 'findAll').mockResolvedValue(result);
 
@@ -47,7 +48,10 @@ describe('UserController', () => {
   describe('create', () => {
     it('should create a user and return the user', async () => {
       const userData = { email: 'new@example.com', username: 'newuser' };
-      const result: User = { ...userData, role: 'brand' };
+      const result = {
+        ...userData,
+        role: 'brand',
+      } as unknown as User;
       jest.spyOn(service, 'createUser').mockResolvedValue(result);
 
       expect(await controller.create(userData)).toEqual(result);
@@ -70,17 +74,28 @@ describe('UserController', () => {
 
   describe('getUserType', () => {
     it('should return user type based on role', async () => {
-      const user = { email: 'test@example.com', role: 'influencer' };
+      const user = {
+        email: 'test@example.com',
+        role: 'brand',
+        username: 'testuser',
+        password: 'hashedpassword',
+        _id: new mongoose.Types.ObjectId(),
+      } as unknown as User;
       jest.spyOn(service, 'findUserByEmail').mockResolvedValue(user);
-
       const result = await controller.getUserType('test@example.com');
       expect(result).toEqual({ type: 'influencer' });
     });
 
     it('should return "brand" if user role is not influencer', async () => {
-      const user = { email: 'test@example.com', role: 'brand' };
+      const user = {
+        email: 'test@example.com',
+        role: 'brand',
+        username: 'testuser',
+        password: 'hashedpassword',
+        _id: new mongoose.Types.ObjectId(),
+      } as unknown as User;
+      
       jest.spyOn(service, 'findUserByEmail').mockResolvedValue(user);
-
       const result = await controller.getUserType('test@example.com');
       expect(result).toEqual({ type: 'brand' });
     });
