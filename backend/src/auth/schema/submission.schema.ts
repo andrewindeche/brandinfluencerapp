9b74c31fc012ba9c43d3d9ec4a4a106f85c54528
andrewindeche <indecheandrew@gmail.com>
2025-04-22 13:32:55 +0300

    use schema decorator

--- backend/src/auth/schema/submission.schema.ts
+++ backend/src/auth/schema/submission.schema.ts
@@ -1,7 +1,7 @@
-import { Prop, Schema as NestSchema, SchemaFactory } from '@nestjs/mongoose';
+import { Prop, Schema as Schema, SchemaFactory } from '@nestjs/mongoose';
 import { Document, Schema as MongooseSchema } from 'mongoose';
 
-@NestSchema()
+@Schema()
 export class Submission extends Document {
   @Prop({ type: MongooseSchema.Types.ObjectId, ref: 'Campaign' })
   campaign: string;
