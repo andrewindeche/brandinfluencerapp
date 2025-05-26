3df321124893a0432bcf4f412fa172c161fa226d
andrewindeche <indecheandrew@gmail.com>
2025-05-26 13:00:16 +0300

    add limit size of parsed objects

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -9,6 +9,7 @@ import * as dotenv from 'dotenv';
 import { MongoExceptionFilter } from '../filters/mongo-exception.filter';
 import { AllExceptionsFilter } from '../filters/all-exceptions.filter';
 import { AdminService } from './admin/admin.service';
+import * as bodyParser from 'body-parser';
 
 dotenv.config();
 
@@ -20,13 +21,14 @@ if (!process.env.JWT_SECRET) {
 
 export async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-    app.enableCors({
+  app.enableCors({
     origin: ['http://localhost:3000', 'http://127.0.0.1:3000'],
     methods: 'GET,POST,PUT,PATCH,DELETE,OPTIONS',
     allowedHeaders: ['Content-Type', 'Authorization'],
     credentials: true,
   });
-  
+  app.use(bodyParser.json({ limit: '5mb' }));
+  app.use(bodyParser.urlencoded({ limit: '5mb', extended: true }));
   const adminService = app.get(AdminService);
   await adminService.bootstrapSuperUserFromEnv();
   const redisClient = new Redis({
