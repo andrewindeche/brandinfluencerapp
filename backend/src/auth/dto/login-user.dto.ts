6c7810d0461e2fb4858b73c9be10f10f6b2edf18
andrewindeche <indecheandrew@gmail.com>
2025-06-02 17:37:46 +0300

    validate user using email

--- backend/src/auth/dto/login-user.dto.ts
+++ backend/src/auth/dto/login-user.dto.ts
@@ -3,7 +3,7 @@ import { IsNotEmpty, IsString } from 'class-validator';
 export class LoginUserDto {
   @IsNotEmpty({ message: 'Username is required' })
   @IsString({ message: 'Username must be a string' })
-  username: string;
+  email: string;
 
   @IsNotEmpty({ message: 'Password is required' })
   @IsString({ message: 'Password must be a string' })
