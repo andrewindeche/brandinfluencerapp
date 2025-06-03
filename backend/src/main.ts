d7fd84449626588c5fde9c7e3cc3b0185b8f1183
andrewindeche <indecheandrew@gmail.com>
2025-06-03 14:37:33 +0300

    Add more urls to cors

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -22,7 +22,7 @@ if (!process.env.JWT_SECRET) {
 export async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.enableCors({
-    origin: ['http://localhost:3000', 'http://127.0.0.1:3000'],
+    origin: ['http://localhost:3000', 'http://127.0.0.1:3000','http://localhost:3001', 'http://127.0.0.1:3001'],
     methods: 'GET,POST,PUT,PATCH,DELETE,OPTIONS',
     allowedHeaders: ['Content-Type', 'Authorization'],
     credentials: true,
