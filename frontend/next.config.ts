514efdfae1723cbd431b0344dae190f1c600d617
andrewindeche <indecheandrew@gmail.com>
2025-06-20 12:34:26 +0300

    update next config

--- frontend/next.config.ts
+++ frontend/next.config.ts
@@ -4,6 +4,10 @@ const nextConfig: NextConfig = {
   env: {
     NEXT_PUBLIC_API_URL: 'http://localhost:4000',
   },
+  images: {
+    domains: ['localhost'],
+    unoptimized: true,
+  },
 };
 
 export default nextConfig;
