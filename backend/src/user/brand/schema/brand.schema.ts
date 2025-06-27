13a9a63fde704bcb0d3c6dd6dac09137b6244c5b
andrewindeche <indecheandrew@gmail.com>
2025-06-27 09:42:18 +0300

    remove unused props for model

--- backend/src/user/brand/schema/brand.schema.ts
+++ backend/src/user/brand/schema/brand.schema.ts
@@ -2,27 +2,14 @@ import { Schema } from 'mongoose';
 import { User, UserModel } from '../../user.schema';
 
 export const BrandSchema = new Schema({
-  description: { type: String },
-  website: { type: String },
   bio: { type: String },
   profileImage: { type: String },
-  logoUrl: { type: String },
-  contactInfo: {
-    phone: { type: String },
-    address: { type: String },
-  },
 });
 
 export interface Brand extends User {
-  description?: string;
   website?: string;
-  logoUrl?: string;
   bio: string;
   profileImage?: string;
-  contactInfo?: {
-    phone?: string;
-    address?: string;
-  };
   role: 'brand';
   createdAt?: Date;
   updatedAt?: Date;
