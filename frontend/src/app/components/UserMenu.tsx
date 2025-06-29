93d47c091af4382d3715db9255bf1e67fd0d5cba
andrewindeche <indecheandrew@gmail.com>
2025-06-29 15:16:22 +0300

    add interface for username and imagesrc types

--- frontend/src/app/components/UserMenu.tsx
+++ frontend/src/app/components/UserMenu.tsx
@@ -2,7 +2,17 @@ import React, { useState, useRef, useEffect } from 'react';
 import Image from 'next/image';
 import { authStore } from '@/rxjs/authStore';
 
-const UserMenu: React.FC<{ onLogout?: () => void }> = ({ onLogout }) => {
+interface UserMenuProps {
+  userName?: string;
+  imageSrc?: string;
+  onLogout?: () => void;
+}
+
+const UserMenu: React.FC<UserMenuProps> = ({
+  userName,
+  imageSrc,
+  onLogout,
+}) => {
   const [isOpen, setIsOpen] = useState(false);
   const [user, setUser] = useState(authStore.getCurrentUser());
   const timeoutRef = useRef<NodeJS.Timeout | null>(null);
@@ -35,8 +45,8 @@ const UserMenu: React.FC<{ onLogout?: () => void }> = ({ onLogout }) => {
         <div className="w-16 h-16 rounded-full border border-black bg-black/40 flex items-center justify-center">
           <div className="w-12 rounded-md border-1 border-black h-14">
             <Image
-              src={user.profileImage || '/images/image4.png'}
-              alt={user.username || 'User'}
+              src={imageSrc || '/images/image4.png'}
+              alt={userName || 'User'}
               width={60}
               height={60}
               className="w-full h-full object-cover"
