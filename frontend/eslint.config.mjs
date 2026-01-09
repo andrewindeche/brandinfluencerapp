b89f15d7551df9ed6685feabd08e27d4aa194f12
andrewindeche <indecheandrew@gmail.com>
2026-01-09 11:30:29 +0300

    remove flat compat

--- frontend/eslint.config.mjs
+++ frontend/eslint.config.mjs
@@ -1,20 +1,9 @@
-import { dirname } from 'path';
-import { fileURLToPath } from 'url';
-import { FlatCompat } from '@eslint/eslintrc';
-
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = dirname(__filename);
-
-const compat = new FlatCompat({
-  baseDirectory: __dirname,
-});
+import next from 'eslint-config-next';
+import prettier from 'eslint-config-prettier';
 
 const eslintConfig = [
-  ...compat.extends(
-    'next/core-web-vitals',
-    'next/typescript',
-    'plugin:prettier/recommended',
-  ),
+  ...next,
+  prettier,
 ];
 
 export default eslintConfig;
