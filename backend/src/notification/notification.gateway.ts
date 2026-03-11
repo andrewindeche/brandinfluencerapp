4786208a49e9ac9eaa96f80310431eedb36c576e
andrewindeche <indecheandrew@gmail.com>
2026-03-11 13:57:00 +0300

    create notification gateway

---
+++ backend/src/notification/notification.gateway.ts
@@ -0,0 +1,19 @@
+import {
+  WebSocketGateway,
+  WebSocketServer,
+} from '@nestjs/websockets';
+import { Server } from 'socket.io';
+
+@WebSocketGateway({ cors: true })
+export class NotificationGateway {
+  @WebSocketServer()
+  server: Server;
+
+  sendToBrand(campaignId: string, payload: any) {
+    this.server.to(`brand-${campaignId}`).emit('submission-event', payload);
+  }
+
+  sendToInfluencer(influencerId: string, payload: any) {
+    this.server.to(`influencer-${influencerId}`).emit('submission-event', payload);
+  }
+}
