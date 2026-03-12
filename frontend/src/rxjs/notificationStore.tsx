4a15f8097349ca14430c58105810263b6c16ad96
andrewindeche <indecheandrew@gmail.com>
2026-03-12 08:55:58 +0300

    add streams for brand and influencer notification

--- frontend/src/rxjs/notificationStore.tsx
+++ frontend/src/rxjs/notificationStore.tsx
@@ -2,11 +2,23 @@ import { BehaviorSubject } from 'rxjs';
 import { NotificationType } from '../interfaces';
 
 const notifications$ = new BehaviorSubject<NotificationType[]>([]);
+const influencerNotifications$ = new BehaviorSubject<NotificationType[]>([]);
+const brandNotifications$ = new BehaviorSubject<NotificationType[]>([]);
 
 export const notificationStore = {
   notifications$,
+  influencerNotifications$,
+  brandNotifications$,
   addNotification(notification: NotificationType) {
     const current = notifications$.getValue();
     notifications$.next([notification, ...current]);
+  },
+  addInfluencerNotification(notification: NotificationType) {
+    const current = influencerNotifications$.getValue();
+    influencerNotifications$.next([notification, ...current]);
+  },
+  addBrandNotification(notification: NotificationType) {
+    const current = brandNotifications$.getValue();
+    brandNotifications$.next([notification, ...current]);
   },
 };
