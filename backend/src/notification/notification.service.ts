23652260562d2bffda6c813ce1cc2f78481649d7
andrewindeche <indecheandrew@gmail.com>
2026-03-17 10:06:22 +0300

    inject notificationgateway

--- backend/src/notification/notification.service.ts
+++ backend/src/notification/notification.service.ts
@@ -1,16 +1,24 @@
 import { Injectable, OnModuleInit } from '@nestjs/common';
 import { KafkaService } from '../kafka/kafka.service';
+import { NotificationGateway } from './notification.gateway';
 
 @Injectable()
 export class NotificationService implements OnModuleInit {
-  constructor(private readonly kafkaService: KafkaService) {}
+  constructor(
+    private readonly kafkaService: KafkaService,
+    private readonly notificationGateway: NotificationGateway
+  ) {}
 
   async onModuleInit() {
     await this.kafkaService.subscribe(
       'submission-events',
       async (key, payload) => {
+        console.log('Kafka event received:', key, payload);
         switch (key) {
           case 'submission.created':
+             this.notificationGateway.sendToUser(payload.brandId, {
+              key,
+              payload,
             break;
           case 'submission.accepted':
             break;
